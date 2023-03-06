b75695b00a4df64ae797f30b4876e60db65a7f21
MDMS29 <mdmazo30@misena.edu.co>
2023-03-06 15:53:50 -0500

    breve

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,6 +1,9 @@
 function mostrar(){
     document.getElementById('formu').style.display = 'block';
+    document.getElementById('formu2').style.display = 'block';
 }
 function cerrar(){
     document.getElementById('formu').style.display = 'none';
+    document.getElementById('formu2').style.display = 'none';
+
 }
